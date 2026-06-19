2bff052af7204b8d708dd73750f00c0a6ba60315
hellopeptiera <hellopeptiera@gmail.com>
2026-06-20 01:05:20 +0800

    Create page.tsx

---
+++ app/library/injection-site/page.tsx
@@ -0,0 +1,19 @@
+export default function InjectionSites() {
+  return (
+    <div>
+      <h1>💉 Injection Sites</h1>
+      <h2>Subcutaneous</h2>
+      <ul>
+        <li>Abdomen</li>
+        <li>Thigh</li>
+        <li>Upper arm</li>
+      </ul>
+      <h2>Intramuscular</h2>
+      <ul>
+        <li>Deltoid</li>
+        <li>Vastus lateralis</li>
+        <li>Glute</li>
+      </ul>
+    </div>
+  );
+}
