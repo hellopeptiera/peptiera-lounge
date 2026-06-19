387aefce0f89379b446fee51d927ad83585795d6
hellopeptiera <hellopeptiera@gmail.com>
2026-06-19 23:14:22 +0800

    Update DashboardCards.tsx

--- app/DashboardCards.tsx
+++ app/DashboardCards.tsx
@@ -1,24 +1,7 @@
-import HeroBanner from "./HeroBanner";
-import DashboardCards from "./DashboardCards";
-export default function Home() {
+export default function DashboardCards() {
   return (
-    <main
-      style={{
-        maxWidth: "1400px",
-        margin: "0 auto",
-        padding: "20px",
-      }}
-    >
-      <HeroBanner />
-      <h1
-        style={{
-          color: "#6D2DB8",
-          marginBottom: "20px",
-        }}
-      >
-        Quick Access
-      </h1>
-      <DashboardCards />
-    </main>
+    <div>
+      Dashboard Cards
+    </div>
   );
 }
