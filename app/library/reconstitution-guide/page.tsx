f0f5e2d7ba54b8f4b64e94a1d8946bfaac3c5678
hellopeptiera <hellopeptiera@gmail.com>
2026-06-20 01:04:33 +0800

    Create page.tsx

---
+++ app/library/reconstitution-guide/page.tsx
@@ -0,0 +1,19 @@
+export default function ReconstitutionGuide() {
+  return (
+    <div>
+      <h1>🧪 Reconstitution Guide</h1>
+      <p>
+        Use bacteriostatic water.
+      </p>
+      <p>
+        Inject slowly along the vial wall.
+      </p>
+      <p>
+        Do not shake vigorously.
+      </p>
+      <p>
+        Refrigerate after mixing.
+      </p>
+    </div>
+  );
+}
